1c72c1ed0f3c7383e68a99bbb986df44b13852f7
Anlor <chenxiex@outlook.com>
2023-07-06 09:29:24 +0800

    添加任务3

--- code/computer.h
+++ code/computer.h
@@ -1,10 +1,36 @@
 #include <string.h>
 #include "../include/playerbase.h"
 #include <stdio.h>
-#include <iostream>
+#include <stdbool.h>
 
+int priority[20][20];
+void set_corners(int x, int y) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            priority[x + i][y + j] = 1;
+        }
+    }
+}
 void init(struct Player *player) {
 	// This function will be executed at the begin of each game, only once.
+    int row = player->row_cnt;
+    int col = player->col_cnt;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            priority[i][j] = 4;
+        }
+    }
+    set_corners(0, 0), set_corners(0, col - 2);
+    set_corners(row - 2, 0), set_corners(row - 2, col - 2);
+    priority[0][0] = priority[0][col-1] = priority[row-1][0] = priority[row-1][col-1] = 5;
+    for (int i = 2; i < col - 2; i++) {
+        priority[0][i] = priority[row - 1][i] = 3;
+        priority[1][i] = priority[row - 2][i] = 2;
+    }
+    for (int i = 2; i < row - 2; i++) {
+        priority[i][0] = priority[i][col - 1] = 3;
+        priority[i][1] = priority[i][col - 2] = 2;
+    }
 }
 
 int is_valid(struct Player *player, int posx, int posy) {
@@ -39,29 +65,15 @@ int is_valid(struct Player *player, int posx, int posy) {
 }
 
 struct Point place(struct Player *player) {
-	for (int i = player->row_cnt - 1; i >= player->row_cnt - i - 1; i--) {
-        int ti = i;
-		for (int j = player->col_cnt - 1; j >= player->col_cnt - j - 1; j--) {
-            int tj = j;
-			if (is_valid(player, ti, tj)) {
-				return initPoint(ti, tj);
-			}
-            tj = player->col_cnt - j - 1;
-            if (is_valid(player, ti, tj)) {
-				return initPoint(ti, tj);
-			}
-		}
-        ti = player->row_cnt - i - 1;
-        for (int j = player->col_cnt - 1; j >= player->col_cnt - j - 1; j--) {
-            int tj = j;
-            if (is_valid(player, ti, tj)) {
-                return initPoint(ti, tj);
-            }
-            tj = player->col_cnt - j - 1;
-            if (is_valid(player, ti, tj)) {
-                return initPoint(ti, tj);
+    struct Point point = initPoint(-1, -1);
+    int max_value = -1;
+    for (int i = 0; i < player->row_cnt; i++) {
+        for (int j = 0; j < player->col_cnt; j++) {
+            if (is_valid(player, i, j) && priority[i][j] > max_value) {
+                max_value = priority[i][j];
+                point = initPoint(i, j);
             }
         }
-	}
-	return initPoint(-1, -1);   // give up
+    }
+	return point;
 }
